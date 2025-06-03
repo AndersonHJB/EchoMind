6219b70b3692865751beacee37b03296693e16d5
AndersonHJB <bornforthis@bornforthis.cn>
2025-06-03 20:34:47 +0800

    新增 ContactTest.py 文件，包含基本的文件头信息

---
+++ V0.1/TestCode/OllamaAPiTest.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2025/6/1 14:46
+# @Author  : AI悦创
+# @FileName: ContactTest.py
+# @Software: PyCharm
+# @Blog    ：https://bornforthis.cn/
+# code is far away from bugs with the god animal protecting
+#    I love animals. They taste delicious.
