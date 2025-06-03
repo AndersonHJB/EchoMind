0ad0038b5692171194033c22878dd66c9d3b5484
AndersonHJB <bornforthis@bornforthis.cn>
2025-06-03 20:34:58 +0800

    Add Ollama API test script and project configuration files
    - Created OllamaAPiTest.py to test the Ollama API with a sample request.
    - Added project configuration files for PyCharm, including module, inspection profiles, and VCS settings.
    - Configured Python SDK to use Python 3.10 for the project.

--- V0.1/TestCode/OllamaAPiTest.py
+++ V0.1/TestCode/OllamaAPiTest.py
@@ -1,8 +1,19 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2025/6/1 14:46
 # @Author  : AI悦创
-# @FileName: ContactTest.py
+# @FileName: OllamaAPiTest.py
 # @Software: PyCharm
 # @Blog    ：https://bornforthis.cn/
 # code is far away from bugs with the god animal protecting
 #    I love animals. They taste delicious.
+import requests
+
+API = "http://localhost:11434/api/generate"
+payload = {
+    "model": "deepseek-r1:7b",
+    "prompt": "给我一个贪吃蛇的网页版游戏",
+    "stream": False
+}
+res = requests.post(API, json=payload)
+print(res.json())
+
